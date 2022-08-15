859ed6dc79d1ad8db41e97b558eaad041453292e
Hannah Giovanna Dawson <karakkaraz@gmail.com>
2022-08-16 00:26:24 +0100

    Project fix-up commit
    A number of major fix-ups added:
    
    1. Revisions to the assembly definitions to allow the code to actually compile
    2. Revision to package structure
    3. Some more scripts
    4. Some fixes to scripts, in particular to LobbyManger.

--- Scripts/Game/GameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using FairlySadProductions.CoreScripts.Scripts.Base;
-
-namespace FairlySadProductions.CoreScripts.Scripts.Game
-{
-    public abstract class GameManager : NetworkedUdonSharpBehaviour
-    {
-        public abstract void TryStartGameWith(int[] players);
-    }
-}
